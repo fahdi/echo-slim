03280d24694e5c602b94ee179c54b45cff9ae7c7
Fahad Murtaza <fahdim@gmail.com>
2021-01-12 04:02:41 +0400

    Init the echo API

---
+++ app/routes.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+use App\Application\Actions\User\ListUsersAction;
+use App\Application\Actions\User\ViewUserAction;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\App;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+
+return function (App $app) {
+    $app->options('/{routes:.*}', function (Request $request, Response $response) {
+        // CORS Pre-Flight OPTIONS Request Handler
+        return $response;
+    });
+
+    $app->get('/', function (Request $request, Response $response) {
+        $response->getBody()->write('Hello world!');
+        return $response;
+    });
+
+	$app->post('/echo', function (Request $request, Response $response) {
+		$response = $response->withHeader('Content-type', 'application/json');
+		$response->getBody()->write(json_encode($_POST));
+		return $response;
+	});
+
+    $app->group('/users', function (Group $group) {
+        $group->get('', ListUsersAction::class);
+        $group->get('/{id}', ViewUserAction::class);
+    });
+};
